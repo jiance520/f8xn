f84b8eb662ad250c4ba8d54db8253b893496ecdb
jiance520 <jiance520@163.com>
2019-09-08 15:34:09 +0800

    定时更新+工具类

--- common/src/main/java/com/utils/TestPattern.java
+++ common/src/main/java/com/utils/TestPattern.java
@@ -6,16 +6,18 @@ public class TestPattern {
     public static void main(String[] args) {
 //        Boolean isTwoNum = Pattern.matches("a(.*|\n)*","a\na\n\n");
 //        Boolean isTwoNum = Pattern.matches("java[\\s\\w-\\.:/\n]*-overwrite","java -jar D:/workspace/idea/guo/milmajordb2012127/src/main/resources/mybatisGenerator/mybatis-generator-core-1.3.2.jar-overwrite");
-        Boolean isTwoNum = Pattern.matches("java[\\s\\w-\\.:/]*-overwrite(.*|\n)*","java -jar D:/workspace/idea/guo/milmajordb2012127/src/main/resources/mybatisGenerator/mybatis-generator-core-1.3.2.jar -configfile D:/workspace/idea/guo/milmajordb2012127/src/main/resources/mybatisGenerator/generator.xml -overwrite\n" +
-                "exit\n" +
-                "exit\n" +
-                "exit");
-        System.out.println("-----isTwoNum:"+isTwoNum);
-        StringBuffer stringBufferRun = new StringBuffer("java -jar D:/workspace/idea/guo/milmajordb2012127/src/main/resources/mybatisGenerator/mybatis-generator-core-1.3.2.jar -configfile D:/workspace/idea/guo/milmajordb2012127/src/main/resources/mybatisGenerator/generator.xml -overwrite\n" +
-                "exit\n" +
-                "exit");
-        String runBatStr = stringBufferRun.toString();
-        runBatStr = runBatStr.replaceAll("java[\\s\\w-\\.:/\n]*-overwrite[\\sa-z\n]*","java -jar -overwrite\nexit");
-        System.out.println("-----runBatStr:"+runBatStr);
+        String oldStr = "#allow 192.168.0.0/16";
+        String regEx = "#allow [0-9\\./]*";
+        String newStr = "#allow 10/8";
+        Boolean flag = Pattern.matches(regEx,oldStr);
+        if(flag){
+            StringBuffer stringBufferRun = new StringBuffer(oldStr);
+            String str = stringBufferRun.toString();
+            str = str.replaceAll(oldStr,newStr);
+            System.out.println("-----str:"+str);
+        }
+        else {
+            System.out.println("-----没有匹配到");
+        }
     }
 }
